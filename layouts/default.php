961b90eb77ce129d55f11e2e61a3cae308a47b9e
Joshua Britt <joshua@britt.email>
2019-12-20 16:17:34 -0600

    Added favicon and description placeholders.

--- layouts/default.php
+++ layouts/default.php
@@ -1,8 +1,10 @@
 <html>
   <head>
     <meta charset="UTF-8" />
+    <meta name="description" content="Blog Site">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Blog Site</title>
+    <link rel="icon" type="image/png" href="<?= path('images/favicon.png'); ?>" />
     <?= STYLES; ?>
   </head>
   <body>
