4a061b037fc8aabf4058f76b9a33ab378843a96a
Joshua Britt <joshua@britt.email>
2020-07-09 18:39:36 -0500

    Updated helpers to latest version.

--- helpers/index/readtime.php
+++ helpers/index/readtime.php
@@ -16,7 +16,7 @@ return function($content, $wpm = 250, $suffix = 'min read') {
 
   $time = ceil(str_word_count($content) / $wpm);
 
-  return "$time $suffix";
+  return "{$time} {$suffix}";
 };
 
 ?>
--- helpers/markdown.php
+++ helpers/markdown.php
@@ -1,43 +1,58 @@
 <?php
 
 /**
- * Markdown 20.06.5 Arcane Helper
+ * Markdown 20.07.1 Arcane Helper
  * MIT https://helpers.arcane.dev
 **/
 
 return function($content, $replace = []) {
-  if(!is_array($content)) {
-    $content = trim($content);
-
-    if(substr($content, -2) === 'md') {
-      if(is_file($path = path($content, true))) {
-        $content = file_get_contents($path);
-      }
-    }
-
-    if(!empty($replace)) {
-      $content = strtr($content, $replace);
-    }
-
-    $content = explode("\n", $content);
-  } else {
-    if(!empty($replace)) {
-      $content = array_map(function($line) use($replace) {
-        return strtr($line, $replace);
-      }, $content);
+  if(is_array($content)) {
+    $content = implode("\n", $content);
+  } else if(substr(rtrim($content), -2) === 'md') {
+    if(is_file($path = path(trim($content), true))) {
+      $content = file_get_contents($path);
     }
   }
 
-  $content = array_values(array_filter($content, 'rtrim'));
+  if(!empty($replace)) {
+    $content = strtr($content, $replace);
+  }
+
+  $includes = strpos($content, "@\x20");
+  $content = explode("\n", $content);
+
+  if($includes !== false) {
+    $includes = preg_grep("/^\s*(?<!\s{4})@\s+(.+)$/", $content);
+
+    foreach(array_reverse($includes, true) as $index => $include) {
+      if(is_file($path = path(trim($include, "@\x20"), true))) {
+        $include = file_get_contents($path);
+  
+        if(substr($path, -2) === 'md') {
+          $include = array_filter(explode("\n", $include));
+        } else {
+          $spaces = str_repeat("\x20", 4);
+          $include = preg_replace("/^/", $spaces, $include);
+        }
+      }
+
+      array_splice($content, $index, 1, $include);
+    }
+  }
+
+  $content = array_map('rtrim', $content);
+  $content = array_values(array_filter($content));
 
   foreach($content as $index => $line) {
     $next = next($content);
-    $linestart = ltrim($line)[0] ?? 0;
-    $nextstart = ltrim($next)[0] ?? 0;
+
+    foreach(['line', 'next'] as $variable) {
+      $start[$variable] = ltrim($$variable)[0] ?? 0;
+    }
 
     unset($format);
 
-    if($linestart === '>') {
+    if($start['line'] === '>') {
       $line = substr($line, strpos($line, '>') + 1);
 
       if(!isset($quote)) {
@@ -56,59 +71,57 @@ return function($content, $replace = []) {
         $format = "\n%s";
       }
     } else {
-      preg_match('/^[\s]*(\#{1,6}|\+|\-|\*)\s+(.+)$/', $line, $part);
+      preg_match("/^\s*(\#{1,6}|\+|\-|\*)\s+(.+)$/", $line, $matches);
 
-      if(!empty($part)) {
-        $partstart = $part[1];
+      if(!empty($matches)) {
+        list($element, $line) = [$matches[1], $matches[2]];
 
-        if($partstart[0] === '#') {
-          $length = strlen($partstart);
+        if($element[0] === '#') {
+          $length = strlen($element);
           $format = "<h{$length}>%s</h{$length}>";
-        } else if(in_array($partstart, ['+', '-', '*'])) {
+        } else if(in_array($element, ['+', '-', '*'])) {
           $format = "<li>%s</li>";
 
-          if(!isset($primary) || !isset($secondary)) {
-            $list = $partstart === '*' ? 'ul' : 'ol';
+          if(!isset($parent) || !isset($child)) {
+            $list = $element === '*' ? 'ul' : 'ol';
 
-            if($partstart === '-') {
+            if($element === '-') {
               $list = "{$list} type=\"A\"";
             }
 
-            if(!isset($primary)) {
+            if(!isset($parent)) {
               $format = "<{$list}>{$format}";
-              $primary = $partstart;
+              $parent = $element;
             }
 
-            if(!isset($secondary)) {
-              if($linestart !== $primary) {
+            if(!isset($child)) {
+              if($start['line'] !== $parent) {
                 $format = "<{$list}>{$format}";
-                $secondary = $partstart;
+                $child = $element;
               }
             }
           }
 
-          if(isset($secondary) || isset($primary)) {
-            if(trim($next[0]) || !$next) {
-              if(isset($secondary)) {
-                if($nextstart !== $secondary) {
-                  $list = $secondary === '*' ? 'ul' : 'ol';
+          if(isset($child) || isset($parent)) {
+            if(!$next || trim($next[0])) {
+              if(isset($child)) {
+                if($start['next'] !== $child) {
+                  $list = $child === '*' ? 'ul' : 'ol';
                   $format = "{$format}</{$list}>";
 
-                  unset($secondary);
+                  unset($child);
                 }
               }
 
-              if($nextstart !== $primary) {
-                $list = $primary === '*' ? 'ul' : 'ol';
+              if($start['next'] !== $parent) {
+                $list = $parent === '*' ? 'ul' : 'ol';
                 $format = "{$format}</{$list}>";
 
-                unset($primary);
+                unset($parent);
               }
             }
           }
         }
-
-        $line = $part[2];
       } else {
         $line = ltrim($line);
 
@@ -120,12 +133,12 @@ return function($content, $replace = []) {
       }
 
       foreach([
-        '*' => '/(\A|\W)\*(?! )([^\*]+)(?<! )\*(?=\W|\Z)/U',
-        '_' => '/(\A|\W)\_(?! )([^\_]+)(?<! )\_(?=\W|\Z)/U',
-        '~' => '/(\A|\W)\~(?! )([^\~]+)(?<! )\~(?=\W|\Z)/U',
-        '`' => '/(\A|\W)\`(?! )([^\`]+)(?<! )\`(?=\W|\Z)/U',
-        '![' => '/(\A|\W)\!\[(.*)\]\((.*)\)(?=\W|\Z)/U',
-        '](' => '/(\A|\W)\[(.*)\]\((.*)\)(?=\W|\Z)/U'
+        '*' => "/(\A|\W)\*(?!\s)([^\*]+)(?<!\s)\*(?=\W|\Z)/U",
+        '_' => "/(\A|\W)\_(?!\s)([^\_]+)(?<!\s)\_(?=\W|\Z)/U",
+        '~' => "/(\A|\W)\~(?!\s)([^\~]+)(?<!\s)\~(?=\W|\Z)/U",
+        '`' => "/(\A|\W)\`(?!\s)([^\`]+)(?<!\s)\`(?=\W|\Z)/U",
+        '![' => "/(\A|\W)\!\[(.*)\]\((.*)\)(?=\W|\Z)/U",
+        '](' => "/(\A|\W)\[(.*)\]\((.*)\)(?=\W|\Z)/U"
       ] as $search => $regex) {
         if(strpos($line, $search) !== false) {
           if($search === '`') {
@@ -136,11 +149,11 @@ return function($content, $replace = []) {
             }, $line);
           } else {
             $html = [
-              '*' => '$1<strong>$2</strong>$3',
-              '_' => '$1<em>$2</em>$3',
-              '~' => '$1<strike>$2</strike>$3',
-              '![' => '$1<img src="$3" alt="$2" />$4',
-              '](' => '$1<a href="$3">$2</a>$4'
+              '*' => "$1<strong>$2</strong>$3",
+              '_' => "$1<em>$2</em>$3",
+              '~' => "$1<strike>$2</strike>$3",
+              '![' => "$1<img src=\"$3\" alt=\"$2\" />$4",
+              '](' => "$1<a href=\"$3\">$2</a>$4"
             ];
 
             $line = preg_replace($regex, $html[$search], $line);
@@ -149,7 +162,7 @@ return function($content, $replace = []) {
       }
     }
 
-    $results[] = sprintf($format ?? "%s", $line);
+    $results[] = sprintf($format ?? '%s', $line);
 
     if(isset($code)) {
       if(!ctype_space(substr($next, 0, 4))) {
@@ -160,7 +173,7 @@ return function($content, $replace = []) {
     }
 
     if(isset($quote)) {
-      if($nextstart !== '>') {
+      if($start['next'] !== '>') {
         $results[] = "</{$quote}>";
 
         unset($quote);
