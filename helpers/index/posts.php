4e363cdb64bab4dc8c4985cf7f1476aae731a7de
Joshua Britt <joshua@britt.email>
2020-07-09 19:07:14 -0500

    Renamed variable for better clarity.

--- helpers/index/posts.php
+++ helpers/index/posts.php
@@ -1,23 +1,25 @@
 <?php
 
 $posts = array_map(function($post) {
-  $array['slug'] = basename($post, '.md');
+  $post = [
+    'path' => $post,
+    'slug' => basename($post, '.md')
+  ];
 
-  if(strpos($array['slug'], '-', 8) == 10) {
+  if(strpos($post['slug'], '-', 8) == 10) {
     $regex = "/^(\d{4}-\d{2}-\d{2})-(.+)$/";
 
-    if(preg_match($regex, $array['slug'], $basename)) {
-      $array['created'] = strtotime($basename[1]);
-      $array['slug'] = $basename[2];
+    if(preg_match($regex, $post['slug'], $basename)) {
+      $post['created'] = strtotime($basename[1]);
+      $post['slug'] = $basename[2];
     }
   }
 
   return array_merge([
-    'created' => filectime($post),
-    'modified' => filemtime($post),
-    'path' => path($post, true)
-  ], $array);
-}, glob('pages/posts/*.md'));
+    'created' => filectime($post['path']),
+    'modified' => filemtime($post['path'])
+  ], $post);
+}, glob(path(['PAGES', 'posts/*.md'], true)));
 
 usort($posts, function($a, $b) {
   return $b['created'] - $a['created'];
